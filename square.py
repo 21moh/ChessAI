9680ad1e33d851690599f71b710354cf8bfffa8b
Matthew Oh <68302300+21moh@users.noreply.github.com>
2023-09-19 17:04:15 -0400

    Update square.py

--- square.py
+++ square.py
@@ -2,15 +2,24 @@ import pygame
 import sys
 
 from const import *
+from piece import Piece
 
 class Square:
     def __init__(self, row, col):
+        self.img = None
+        self.team = None
         self.piece = None
         self.gridRow = row
         self.gridCol = col
         self.attakers = []
-    def add_piece(self, piece, row, col):
+    
+    def add_piece(self, team, piece, img, row, col):
+        self.img = img
+        self.team = team
         self.piece = piece
         self.gridRow = row
         self.gridCol = col
+    
+    def hasPiece(self):
+        return self.piece != None
     
