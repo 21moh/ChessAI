770a22818c56515689a13a920c6e898f48423e9c
Matthew Oh <68302300+21moh@users.noreply.github.com>
2023-09-19 17:04:49 -0400

    Update piece.py

---
+++ piece.py
@@ -0,0 +1,13 @@
+import pygame
+import sys
+
+from const import *
+
+class Piece:
+    def __init__(self):
+        # team
+        # grid location
+        # type of piece
+        # image
+    
+    # different piece definitions holds valid moves
