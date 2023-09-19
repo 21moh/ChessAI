dc2a4de9a44c9afcf139fcf52f31cdf82a76c31d
Matthew Oh <68302300+21moh@users.noreply.github.com>
2023-09-19 17:03:28 -0400

    Update game.py

--- game.py
+++ game.py
@@ -24,4 +24,27 @@ class Game:
                 else:
                     color = (119, 154, 88)
                 pygame.draw.rect(screen, color, (x, y, CELL_SIZE, CELL_SIZE))
+
+
+    def show_pieces(self, screen):
+        board = self.board
+        for i in range(ROWS):
+            for j in range(COLS):
+                if (board[i][j].hasPiece):
+                    piece = board[i][j].
+                    
+                    original_image = pygame.image.load(piece.image)
+                    original_width, original_height = original_image.get_size()
+
+                    spacing_factor = 0.9
+
+                    # Calculate the scaling factors
+                    width_scale = CELL_SIZE * spacing_factor / original_width
+                    height_scale = CELL_SIZE * spacing_factor / original_height
+
+                    # Use the smaller scaling factor to maintain aspect ratio
+                    scale_factor = min(width_scale, height_scale)
+
+                    # Scale the image
+                    piece.image = pygame.transform.scale(original_image, (int(original_width * scale_factor), int(original_height * scale_factor)))
     
