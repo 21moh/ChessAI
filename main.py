0567369306dd756d9d54e547684389f39a7c0a18
Matthew Oh <68302300+21moh@users.noreply.github.com>
2023-09-21 12:03:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class Main:
     def mainloop(self):
         game = self.game
         screen = self.screen
+        dragger = self.game.dragger
 
         #initiate pygame
         #initialize board
@@ -23,9 +24,37 @@ class Main:
         running = True
 
         while running:
+            game.board.initialize_board()
             game.show_board(screen)
+            game.show_pieces(screen)
+            
             for event in pygame.event.get():
-                pass
+                
+                if event.type == pygame.QUIT:
+                    running = False  # Set the flag to exit the loop if the window is closed
+                    pygame.quit()  # Quit Pygame properly
+                    sys.exit()     # exit program quit game
+                
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    dragger.update_mouse(event.pos)
+
+                    clicked_row = dragger.mouseY // CELL_SIZE
+                    clicked_col = dragger.mouseX // CELL_SIZE
+
+                    if game.board.grid[clicked_row][clicked_col].piece != None:
+                        piece = game.board.grid[clicked_row][clicked_col].piece
+                        dragger.save_initial(event.pos)
+                        dragger.drag_piece(piece)
+
+                elif event.type == pygame.MOUSEMOTION:
+                    if dragger.dragging:
+                        dragger.update_mouse(screen)
+                        dragger.update_blit(screen)
+
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    dragger.undrag_piece
+
+
             pygame.display.update()
 
 
