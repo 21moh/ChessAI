6932b0f8851e581e1a04142e7c11a8e6a968bb25
Matthew Oh <68302300+21moh@users.noreply.github.com>
2023-09-21 12:05:46 -0400

    Update square.py

--- square.py
+++ square.py
@@ -6,20 +6,19 @@ from piece import Piece
 
 class Square:
     def __init__(self, row, col):
-        self.img = None
-        self.team = None
         self.piece = None
-        self.gridRow = row
-        self.gridCol = col
+        self.team = None
+        self.img = None
+        self.object = None
+        self.row = row
+        self.col = col
         self.attakers = []
     
     def add_piece(self, team, piece, img, row, col):
-        self.img = img
-        self.team = team
         self.piece = piece
-        self.gridRow = row
-        self.gridCol = col
-    
-    def hasPiece(self):
-        return self.piece != None
+        self.team = team
+        self.img = img
+        self.row = row
+        self.col = col
+        self.object = Piece(self.piece, self.team, self.img, self.row, self.col)
     
