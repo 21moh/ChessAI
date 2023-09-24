7128508c8fe59b0c762910b027e9488b527cb9ef
Matthew Oh <68302300+21moh@users.noreply.github.com>
2023-09-23 22:34:29 -0400

    Update piece.py

--- piece.py
+++ piece.py
@@ -2,23 +2,16 @@ import pygame
 import sys
 
 from const import *
-from piece import Piece
 
-class Square:
-    def __init__(self, row, col):
-        self.piece = None
-        self.team = None
-        self.img = None
-        self.object = None
-        self.row = row
-        self.col = col
-        self.attakers = []
-    
-    def add_piece(self, team, piece, img, row, col):
-        self.piece = piece
+class Piece:
+    def __init__(self, type, team, image, row, col):
+        self.type = type
         self.team = team
-        self.img = img
+        self.image = image
         self.row = row
         self.col = col
-        self.object = Piece(self.piece, self.team, self.img, self.row, self.col)
+        self.connect = None
+        self.possible_moves = []
+
+
 
