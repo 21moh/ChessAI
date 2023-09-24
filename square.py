717ccc405302e6f3c962fe8ee438ee0234c5ffde
Matthew Oh <68302300+21moh@users.noreply.github.com>
2023-09-23 22:33:11 -0400

    Update square.py

--- square.py
+++ square.py
@@ -2,23 +2,99 @@ import pygame
 import sys
 
 from const import *
-from piece import Piece
 
 class Square:
     def __init__(self, row, col):
         self.piece = None
         self.team = None
-        self.img = None
-        self.object = None
+        self.image = None
         self.row = row
         self.col = col
-        self.attakers = []
+        #self.attakers = []
+        self.moves = []
     
     def add_piece(self, team, piece, img, row, col):
         self.piece = piece
         self.team = team
-        self.img = img
+        self.image = img
         self.row = row
         self.col = col
-        self.object = Piece(self.piece, self.team, self.img, self.row, self.col)
+
+
+    def get_moves(self):
+        piece = self.piece
+        row = self.row
+        col = self.col
+
+
+        ##########################################
+        if (piece == "pond"):
+            if (self.team == "white"):
+                
+                
+                #if board[row][grid] diagonal is opposite piece... --> restudy your code.
+
+                if (row == 6):
+                    self.moves.append([row-2, col])
+                    self.moves.append([row-1, col])
+                elif (row >= 1 and row <= 5):
+                    self.moves.append([row-1, col])
+                
+
+            elif (self.team == "black"):
+                if (row == 1):
+                    self.moves.append([row+2, col])
+                    self.moves.append([row+1, col])
+                elif (row >= 2 and row <= 6):
+                    self.moves.append([row+1, col])
+
+
+        ##########################################
+        elif (piece == "bishop"):
+            
+
+            if (self.team == "white"):
+                pass
+
+        
+        ##########################################
+        elif (piece == "knight"):
+            if (row-2 >= 0 and col-1 >= 0):
+                    self.moves.append([row-2, col-1])
+            if (row-2 >= 0 and col+1 <= COLS-1):
+                self.moves.append([row-2, col+1])
+            if (row-1 >= 0 and col-2 >= 0):
+                self.moves.append([row-1, col-2])
+            if (row+1 <= ROWS-1 and col-2 >= 0):
+                self.moves.append([row+1, col-2])
+            if (row+2 <= ROWS-1 and col-1 >= 0):
+                self.moves.append([row+2, col-1])
+            if (row+2 <= ROWS-1 and col+1 <= COLS-1):
+                self.moves.append([row+2, col+1])
+            if (row+1 <= ROWS-1 and col+2 <= COLS-1):
+                self.moves.append([row+1, col+2])
+            if (row-1 >=0 and col+2 <= COLS-1):
+                self.moves.append([row-1, col+2])
+
+            if (self.team == "white"):
+                pass
+
+        ##########################################
+        elif (piece == "rook"):
+            pass
+
+
+        ##########################################
+        elif (piece == "queen"):
+            pass
+
+
+        ##########################################
+        elif (piece == "king"):
+            pass
+
+
+    
+    
+
     
