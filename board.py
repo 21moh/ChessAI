1794b68e6ddb1c702163c929ac4eabd87e4c6108
Matthew Oh <68302300+21moh@users.noreply.github.com>
2023-09-23 22:47:47 -0400

    Update board.py

--- board.py
+++ board.py
@@ -2,54 +2,40 @@ import pygame
 import sys
 
 from const import *
-from board import Board
-from dragger import Dragger
+from piece import Piece
+from square import Square
 
-
-class Game:
+class Board:
     def __init__(self):
-        self.turn = "White"
-        self.board = Board()
-        self.dragger = Dragger()
-    
-    def show_board(self, screen):
-        screen.fill((200, 200, 200))  # White background
-
-        # Draw the grid and pieces
-        for row in range(8):
-            for col in range(8):
-                x = col * CELL_SIZE
-                y = row * CELL_SIZE
-                if (row + col) % 2 == 0:
-                    color = (234, 235, 200)
-                else:
-                    color = (119, 154, 88)
-                pygame.draw.rect(screen, color, (x, y, CELL_SIZE, CELL_SIZE))
-
-
-    def show_pieces(self, screen):
-        board = self.board
+        self.grid = [[None for i in range(8)] for i in range(8)]    #square class, holds current piece and possible attacking pieces
+        
+    def initialize_board(self):
+        grid = self.grid
         for i in range(ROWS):
             for j in range(COLS):
-                if (board.grid[i][j].piece != None):
-                    original_image = pygame.image.load(board.grid[i][j].object.image)
-                    original_width, original_height = original_image.get_size()
+                grid[i][j] = Square(i, j)
+        
+        for i in range(8):
+            grid[1][i].add_piece("black", "pond", "ChessAI/blackPond.png", 1, i)
+            grid[6][i].add_piece("white", "pond", "ChessAI/whitePond.png", 6, i)
+        
+        grid[0][0].add_piece("black", "rook", "ChessAI/blackRook.png", 0, 0)
+        grid[0][7].add_piece("black", "rook", "ChessAI/blackRook.png", 0, 7)
+        grid[7][0].add_piece("white", "rook", "ChessAI/whiteRook.png", 7, 0)
+        grid[7][7].add_piece("white", "rook", "ChessAI/whiteRook.png", 7, 7)
 
-                    spacing_factor = 0.9
+        grid[0][1].add_piece("black", "knight", "ChessAI/blackKnight.png", 0, 1)
+        grid[0][6].add_piece("black", "knight", "ChessAI/blackKnight.png", 0, 6)
+        grid[7][1].add_piece("white", "knight", "ChessAI/whiteKnight.png", 7, 1)
+        grid[7][6].add_piece("white", "knight", "ChessAI/whiteKnight.png", 7, 6)
 
-                    # Calculates scaling factors
-                    width_scale = CELL_SIZE * spacing_factor / original_width
-                    height_scale = CELL_SIZE * spacing_factor / original_height
+        grid[0][2].add_piece("black", "bishop", "ChessAI/blackBishop.png", 0, 2)
+        grid[0][5].add_piece("black", "bishop", "ChessAI/blackBishop.png", 0, 5)
+        grid[7][2].add_piece("white", "bishop", "ChessAI/whiteBishop.png", 7, 2)
+        grid[7][5].add_piece("white", "bishop", "ChessAI/whiteBishop.png", 7, 5)
 
-                    # Use the smaller scaling factor to maintain aspect ratio
-                    scale_factor = min(width_scale, height_scale)
-
-                    # Scales the image
-                    board.grid[i][j].object.image = pygame.transform.scale(original_image, (int(original_width * scale_factor), int(original_height * scale_factor)))
-                    
-                    x = j * CELL_SIZE
-                    y = i * CELL_SIZE
-                    img_x = x + (CELL_SIZE - board.grid[i][j].object.image.get_width()) // 2
-                    img_y = y + (CELL_SIZE - board.grid[i][j].object.image.get_height()) // 2
-                    screen.blit(board.grid[i][j].object.image, (img_x, img_y))
+        grid[0][3].add_piece("black", "queen", "ChessAI/blackQueen.png", 0, 3)
+        grid[0][4].add_piece("black", "king", "ChessAI/blackKing.png", 0, 4)
+        grid[7][3].add_piece("white", "queen", "ChessAI/whiteQueen.png", 7, 3)
+        grid[7][4].add_piece("white", "king", "ChessAI/whiteKing.png", 7, 4)
         
